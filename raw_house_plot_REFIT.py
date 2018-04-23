c876c9fbe009c9a43d14d1629eae82d5379ab2e2
michele <mike_55@hotmail.it>
2018-04-23 10:30:57 +0100

    fridge

--- raw_house_plot_REFIT.py
+++ raw_house_plot_REFIT.py
@@ -16,7 +16,7 @@ building = 'House20'
 filename = path + 'CLEAN_' + building + '.csv'
 
 # Number of sample to plot
-chunksize = 5*10**6
+chunksize = 5*10**5
 
 
 
@@ -31,7 +31,7 @@ for idx, chunk in enumerate(pd.read_csv(filename,
 
     fig2 = plt.figure()
     plt.plot(chunk['Aggregate'])
-    plt.plot(chunk['Appliance5'])
+    plt.plot(chunk['Appliance1'])
     #plt.plot(chunk['Appliance2'])
     #plt.plot(chunk['Appliance3'])
     #plt.plot(chunk['Appliance4'])
